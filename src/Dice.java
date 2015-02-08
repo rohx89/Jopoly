ff921dd16b8443a7a1ed8f38572336d7d0344515
rohx89 <rohx89@192.168.0.12>
2015-02-08 16:36:49 +0100

    Dice created

---
+++ src/Dice.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+/**
+ * This class reperesents a dice.
+ * 
+ * @author rohx89
+ *
+ */
+public class Dice {
+	/**
+	 * The amount of sides
+	 */
+	private int _sides;
+	/**
+	 * The current value of the dice
+	 */
+	private int _currentValue;
+
+	/**
+	 * initializes a dice
+	 * 
+	 * @param sides
+	 *            A Dice must have at least 2 sides
+	 */
+	public Dice(int sides) {
+		assertTrue(
+				"The dice has a wrong amount of sides! A dice must have at least 2 sides",
+				sides > 1);
+		_sides = sides;
+		_currentValue = throwDice();
+		System.out.println("DEBUG: Dice was created");
+	}
+
+	/**
+	 * Sets the _currentValue to a random number between 1 and the amount of
+	 * sides
+	 * 
+	 * @return the current Value of the dice
+	 */
+	public int throwDice() {
+		Random randomGenerator = new Random();
+		_currentValue = randomGenerator.nextInt(_sides - 1) + 1;
+		System.out.println("DEBUG: Dice was thrown. The value of the this is: "
+				+ _currentValue);
+		return _currentValue;
+	}
+
+	/**
+	 * 
+	 * @return the current Value of the dice
+	 */
+	public int getCurrentValue() {
+
+		return _currentValue;
+	}
+}
---
+++ src/DiceTest.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Tests the dice
+ * 
+ * @author rohx89
+ *
+ */
+public class DiceTest {
+
+	/**
+	 * Checks whether a dice with 2 sides is correctly initialize. This means
+	 * that no exception is thrown and the dice has a value which is 1 or 2
+	 */
+	@Test
+	public void initializeDicePositivTest() {
+		Dice dice = new Dice(2);
+		assertNotNull("dice is Null", dice);
+		assertTrue(((dice.getCurrentValue() == 1) || (dice.getCurrentValue() == 2)));
+	}
+
+	/**
+	 * Test wether no Dice can be created,which has a less than 2 sides
+	 */
+	@Test(expected = java.lang.AssertionError.class)
+	public void initializeDiceNegativeTest() {
+		Dice dice = new Dice(1);
+	}
+
+	/**
+	 * Tests wether throwing the dice returns a plausible value (0<value<=sides)
+	 */
+	@Test
+	public void throwDiceTest() {
+		Dice dice = new Dice(6);
+		for (int timesThrown = 0; timesThrown < 1000; timesThrown++) {
+			int value = dice.throwDice();
+			assertTrue(value > 0 && value <= 6);
+		}
+
+	}
+}
